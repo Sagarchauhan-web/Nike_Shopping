77e659776f13ce1c442b785dc5ab888e425650f2
Sagar <Sagarchawan4646@gmail.com>
2023-12-13 15:00:03 +0530

    Popular Products and Super Quality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,11 +6,13 @@ const Button = ({ label, iconURL }) => {
     leading-none bg-coral-red rounded-full text-white'
     >
       {label}
-      <img
-        src={iconURL}
-        alt='arrow right icon'
-        className='ml-2 w-5 h-5 rounded-full'
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt='arrow right icon'
+          className='ml-2 w-5 h-5 rounded-full'
+        />
+      )}
     </button>
   );
 };
